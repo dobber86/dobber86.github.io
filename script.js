b3d770244fdc8f14a6eed4b58d6d2fb09dc1cd42
dobber86 <35963693+dobber86@users.noreply.github.com>
2018-02-04 16:24:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-//Hier komt mijn JavaScript
 
+//	Alle eigenschappen van de teams (objecten) met een class gebruiken. Die kan ik dan gebruiken voor de Stand.
 
-
+/*
 class team = {
 	constructor(name) {
 		this._name = name;
@@ -52,7 +52,46 @@ const teamA = new team('Slagerij');
 const teamB = new team('Autobedrijf');
 const teamC = new team('Sportmassage');
 const teamD = new team('Bouwbedrijf');
+*/
 
 
-teama.innerHTML = teamA.name;
-teamb.innerHTML = teamB.name;
+
+
+//	Ingevoerde namen uitvoeren in de wedstrijden
+
+function editTeam() {		// indrukken opslaan knop teams
+	
+	updateNames("teama");
+	updateNames("teamb");
+	updateNames("teamc");
+	updateNames("teamd");
+}	
+
+function updateNames(team) {
+	
+		let t = document.getElementsByClassName(team);		//variabele voor alle team namen in een klasse
+		
+		let l = team.substring(4, 5);		//team letter pakken
+		l = l.toUpperCase();
+		
+		for (let i = 0; i < t.length; i++) {		//net zo lang uitvoeren tot het aantal keer dat een team voorkomt 
+			t[i].innerHTML = document.getElementById("name_t" + l).value;		//pak de naam uit invoerbox en schrijven
+		}
+		
+		// Spelers
+		for (let j = 1; j < 5; j++) { 		//4 spelers in een team
+			
+			let s = document.getElementsByClassName(`t${l}s${j}`);		//variabele voor alle namen in een klasse
+				
+			for (let i = 0; i < s.length; i++) {		//net zo lang uitvoeren tot het aantal keer dat een speler voorkomt 
+				s[i].innerHTML = document.getElementById(`name_t${l}s${j}`).value;		//pak de naam uit invoerbox en schrijven
+			}
+		}
+		
+}
+
+//	Verwerken van ingevoerde scores
+function editScore() {
+	
+}
+
